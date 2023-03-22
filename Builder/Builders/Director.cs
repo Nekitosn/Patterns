7f1909d76cfbb4b51e77b2eaf42a2ef216f013aa
Nekitosn <nikitasedov57@gmail.com>
2023-03-22 22:56:45 +0200

    feat - Implemented builder pattern; feat - Created computer builder interfaces; feat - Created director, computer builders model; feat - Created computer model; feat - Created an example of how the program works.

---
+++ Builder/Builders/Director.cs
@@ -0,0 +1,20 @@
+﻿using Builder.Builders.Interfaces;
+
+namespace Builder.Builders;
+
+public class Director
+{
+    private IComputerBuilder _computerBuilder;
+    
+    public Director(IComputerBuilder computerBuilder)
+    {
+        _computerBuilder = computerBuilder;
+    }
+    
+    public void ConstructComputer()
+    {
+        _computerBuilder.BuildMemory();
+        _computerBuilder.BuildProcessor();
+        _computerBuilder.BuildMotherBoard();
+    }
+}
---
+++ Builder/Models/Computer.cs
@@ -0,0 +1,15 @@
+﻿namespace Builder.Models;
+
+public class Computer
+{
+    public string Memory { get; set; }
+    public string Processor { get; set; }
+    public string MotherBoard { get; set; }
+    
+    public void Display()
+    {
+        Console.WriteLine($"Memory: {Memory}");
+        Console.WriteLine($"Processor: {Processor}");
+        Console.WriteLine($"MotherBoard: {MotherBoard}");
+    }
+}
