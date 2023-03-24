15b25d041dcbb034da716ae4795eabeda68d5bce
Nekitosn <nikitasedov57@gmail.com>
2023-03-24 21:01:22 +0200

    fix - Commit program cs.

---
+++ Builder/Program.cs
@@ -0,0 +1,21 @@
+﻿using Builder.Builders;
+using Builder.Builders.Interfaces;
+using Builder.Models;
+
+Console.WriteLine("   Starting project\n");
+
+IComputerBuilder topComputerBuilder = new TopComputerBuilder();
+Director topDirector = new Director(topComputerBuilder);
+topDirector.ConstructComputer();
+Computer topComputer = topComputerBuilder.GetComputer();
+Console.WriteLine("   Top computer:");
+topComputer.Display();
+
+IComputerBuilder mediumComputerBuilder = new MediumComputerBuilder();
+Director mediumDirector = new Director(mediumComputerBuilder);
+mediumDirector.ConstructComputer();
+Computer mediumComputer = mediumComputerBuilder.GetComputer();
+Console.WriteLine("\n   Medium computer:");
+mediumComputer.Display();
+
+Console.WriteLine("\n   End project");
