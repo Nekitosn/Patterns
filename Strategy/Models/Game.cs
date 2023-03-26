7b9b0d6567116dbded84c876bf6c489b30912a10
Mykyta <91759478+SedovMykyta@users.noreply.github.com>
2023-03-26 22:07:20 +0300

    Implemented strategy pattern for game #6
    feat - Implemented strategy pattern for game;
    feat - Created abstract model character;
    feat - Created archer, warrior model;
    feat - Created game model;
    feat - Created an example of how the program works.

---
+++ Strategy/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using Strategy.Abstracts;
+
+namespace Strategy.Models;
+
+public class Game 
+{
+    private Character _character;
+
+    public void SetCharacter(Character character) 
+    {
+        _character = character;
+    }
+
+    public void Play() 
+    {
+        _character.Move();
+        _character.Attack();
+    }
+}
