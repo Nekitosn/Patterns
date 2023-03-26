c15d36c717769366ca6fc346e3a00a58bd50a0cf
Mykyta <91759478+SedovMykyta@users.noreply.github.com>
2023-03-26 22:16:56 +0300

    Implemented builder pattern for computer #5
    feat - Implemented builder pattern;
    feat - Created computer builder interfaces;
    feat - Created director, computer builders model;
    feat - Created computer model;
    feat - Created an example of how the program works.

---
+++ Builder/Builders/Interfaces/IComputerBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Builder.Models;
+
+namespace Builder.Builders.Interfaces;
+
+public interface IComputerBuilder
+{
+    public void BuildMemory();
+    public void BuildProcessor();
+    public void BuildMotherBoard();
+    public Computer GetComputer();
+}
---
+++ Builder/Models/Computer.cs
@@ -0,0 +1,16 @@
+﻿namespace Builder.Models;
+
+public class Computer
+{
+    public string Memory { get; set; }
+    public string Processor { get; set; }
+    public string MotherBoard { get; set; }
+
+    public override string ToString()
+    {
+        return 
+            $"\nMemory: {Memory}\n" +
+            $"Processor: {Processor}\n" +
+            $"MotherBoard: {MotherBoard}" ;
+    }
+}
